e0fbbcdc10f4c294e6d58968b9142bcc7843cab4
Nicolas <36539498+hnicolus@users.noreply.github.com>
2020-05-19 23:35:53 +0200

    Add project files.

---
+++ TrackMyWalks/TrackMyWalks/Models/WalkData.cs
@@ -0,0 +1,15 @@
+﻿namespace TrackMyWalks.Models
+{
+    public class WalkData
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Distance { get; set; }
+        public string Difficulty { get; set; }
+        public string ImageUrl { get; set; }
+    } 
+}
+ 
